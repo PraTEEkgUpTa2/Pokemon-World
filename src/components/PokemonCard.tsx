80135bef6d90bea14af39a85b873dd1374d5dfd7
PraTEEkgUpTa2 <97375784+PraTEEkgUpTa2@users.noreply.github.com>
2025-04-09 23:15:33 +0530

    update card

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -12,7 +12,7 @@ interface PokemonCardProps {
 const PokemonCard = ({ name, image, id }: PokemonCardProps) => {
   return (
     <Link href={`/pokemon/${id}`}>
-      <div className="w-full max-w-[250px] mx-auto p-5 min-h-[150px] bg-white/20 dark:bg-gray-700 backdrop-blur-lg rounded-2xl shadow-md hover:shadow-xl hover:scale-[1.03] transition duration-300 cursor-pointer flex flex-col items-center justify-between">
+      <div className="w-full max-w-[250px] mx-auto p-5 min-h-[150px] bg-white/20  backdrop-blur-lg rounded-2xl shadow-md hover:shadow-xl hover:scale-[1.03] transition duration-300 cursor-pointer flex flex-col items-center justify-between">
         {/* Centered Image */}
         <div className="w-full flex justify-center">
           <img src={image} alt={name} className="w-28 h-28 object-contain" />
